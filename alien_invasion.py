fa881ca69dd7f4adbe86ca2c8be0bd99e2bd8aaf
jkaizane <allettekjordan@gmail.com>
2023-07-01 15:55:29 +0100

    Source code build

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,1 +1,28 @@
-print("Hello, fuck face!")
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour."""
+
+    def __init__(self):
+        """Initialise the game, and create game resources."""
+        pygame.init()
+
+        self.screen = pygame.display.set.mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Make the most recently drawn screen visible.
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run.game()
