20326e357aa4ea1002351a0d71ace90317269357
jkaizane <allettekjordan@gmail.com>
2023-10-09 17:47:04 +0100

    Blacjack and bug fixes

--- 10.2_calc.py
+++ 10.2_calc.py
@@ -17,23 +17,24 @@ operations = {
     "/":divide
 }
 
-num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
+def calculator():
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-if symbol == "+":
-    def add():
-elif symbol == "-":
-    def subtract():
-elif symbol == "*":
-    def multiply():
-elif symbol == "/":
-    def divide():
-        
-    
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
 
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-        
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ") == "y":
+        num1 = answer
+    else:
+        should_continue = False
+        calculator()
 
+calculator()
