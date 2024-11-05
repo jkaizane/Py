5413ab37c830dbbc73dad773b2ea8f625c3f06cb
jkaizane <134961308+jkaizane@users.noreply.github.com>
2024-11-05 14:34:36 +0000

    05/11

---
+++ oop_coffee_machine/main.py
@@ -0,0 +1,7 @@
+import oop_coffee_machine.menu.Menu, MenuItem
+import oop_coffee_machine.coffee_maker.CoffeeMaker
+import oop_coffee_machine.money_machine.MoneyMachine
+
+money_machine = MoneyMachine()
+
+money_machine.report()
